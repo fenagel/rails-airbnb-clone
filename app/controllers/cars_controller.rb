e3b9bddc2aaf611cde34f3090562eac845d2a6a2
Felix Nagel <felixna7@gmail.com>
2020-05-26 14:06:09 +0200

    cars controller and model update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,30 +1,45 @@
 class CarsController < ApplicationController
 
   def index
-    @cars = Car.where(car.location = params[:location],  )
+    @cars = Car.all
   end
 
   def show
-
+    @car = Car.find(params[:id])
   end
 
   def new
-
+    @car = Car.new
   end
 
   def create
-
+    @car = Car.new(car_params)
+    if @car.save!
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @car = Car.find(params[:id])
   end
 
   def update
-
+    @car = Car.find(params[:id])
+    @car = Car.update(car_params)
+    redirect_to car_path(@car)
   end
 
   def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to root_path
+  end
 
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :brand, :model, :renting_price, :experience, :location, :photo)
   end
 end
