1020eebe6e869adcab49a1ee20ac487241ec1adf
Lea Grieder <lea@grieder.org>
2020-05-27 12:36:03 +0200

    seeds and start of show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(email: "lea@gjed.org", password: "jhwfebs")
+end
+40.times do
+  Car.create(name: Faker::Vehicle.color, brand: Faker::Vehicle.manufacture,
+             model: Faker::Vehicle.model,
+             renting_price: Faker::Vehicle.year,
+             location: Faker::Address.city,
+             experience: 1, user_id: rand(1..2)
+            )
+end
